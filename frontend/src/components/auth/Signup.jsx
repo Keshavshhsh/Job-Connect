d2396e69a614c98468ea0b1c40b30f5aec9bfbad
naman katiyar <naman.20233196@mnnit.ac.in>
2025-01-12 19:44:44 +0530

    herosection ans input tk done

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -8,6 +8,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { USER_API_END_POINT } from "@/utils/constant";
 import axios from "axios";
 import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import store from "@/redux/store";
+import { setLoading } from "@/redux/authSlice";
 
 const Signup = () => {
   const [input, setInput] = useState({
@@ -18,6 +21,8 @@ const Signup = () => {
     role: "",
     file: "",
   });
+  const {loading}=useSelector(store=>store.auth);
+  const dispatch=useDispatch();
   const navigate=useNavigate();
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
@@ -39,6 +44,7 @@ const Signup = () => {
       formData.append('file',input.file);
     }
     try {
+      dispatch(setLoading(true));
       const res=await axios.post(`${USER_API_END_POINT}/register`,formData,{
         headers:{
           'Content-Type': 'multipart/form-data'
@@ -52,6 +58,9 @@ const Signup = () => {
   } catch (error) {
       console.log(error);
        toast.error(error.response.data.message);
+    }
+    finally{
+      dispatch(setLoading(false));
     }
     
   }
@@ -128,9 +137,16 @@ const Signup = () => {
               className="cursor-pointer" />
             </div>
           </div>
-          <Button type="submit" className="w-full bg-black text-white  my-4">
-            Signup
-          </Button>
+          {loading ? (
+            <Button className="w-full bg-black text-white  my-4">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin " />
+              Please Wait
+            </Button>
+          ) : (
+            <Button type="submit" className="w-full bg-black text-white  my-4">
+              Login
+            </Button>
+          )}
           <span className="text-small">
             Already have an account?{" "}
             <Link to="/login" className="text-blue-600">
