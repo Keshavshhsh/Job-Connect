cf84847232c7b70d610744f3e64462b4a91a2e3a
naman katiyar <naman.20233196@mnnit.ac.in>
2025-01-14 16:00:48 +0530

    job description done

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import React from "react";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 
 const JobDescription = () => {
+  const isApplied = false;
   return (
-    <div>JobDescription</div>
-  )
-}
+    <div className="max-w-7xl mx-auto my-10">
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="font-bold text-xl">Fronted developer</h1>
+          <div className="flex items-center gap-2 mt-4">
+            <Badge className={"text-blue-700 font-bold"} variant="ghost">
+              12 Positions
+            </Badge>
+            <Badge className={"text-[#F83002] font-bold"} variant="ghost">
+              Part Time
+            </Badge>
+            <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
+              47lpa
+            </Badge>
+          </div>
+        </div>
+        <Button
+          disabled={isApplied}
+          variant="outline"
+          className={`rounded-lg ${
+            isApplied
+              ? "bg-gray-600 text-white cursor-not-allowed"
+              : "text-green-400 bg-blue-600"
+          }`}
+        >
+          {isApplied ? "Already applied" : "Apply Now"}
+        </Button>
+      </div>
+      <h1 className="border-b-2 border-b-gray-300 font-medium py-4">
+        Job Description
+      </h1>
+      <div>
+        <h1 className="font-bold my-1">
+          Role:
+          <span className="pl-4 font-normal text-gray-800">
+            Frontend Developer
+          </span>
+        </h1>
+        <h1 className="font-bold my-1">
+          Location:
+          <span className="pl-4 font-normal text-gray-800">
+            hyderabad
+          </span>
+        </h1>
+        <h1 className="font-bold my-1">
+          Description:
+          <span className="pl-4 font-normal text-gray-800">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, nesciunt vel praesentium sint iste dolorem.
+          </span>
+        </h1>
+        <h1 className="font-bold my-1">
+          Experience:
+          <span className="pl-4 font-normal text-gray-800">
+            2 yrs
+          </span>
+        </h1>
+        <h1 className="font-bold my-1">
+          Salary:
+          <span className="pl-4 font-normal text-gray-800">
+            24lpa
+          </span>
+        </h1>
+        <h1 className="font-bold my-1">
+          Total Applicants:
+          <span className="pl-4 font-normal text-gray-800">
+            4
+          </span>
+        </h1>
+        <h1 className="font-bold my-1">
+          Posted Date:
+          <span className="pl-4 font-normal text-gray-800">
+            14-01-2025
+          </span>
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default JobDescription
+export default JobDescription;
