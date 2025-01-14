f488a13be731b93d16d040a9d2df131e3edfb281
naman katiyar <naman.20233196@mnnit.ac.in>
2025-01-14 11:56:16 +0530

    browse page maked

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
-const randomJobs=[1,2,3];
+import Job from './Job';
+const randomJobs=[1,2,3,4,5,6,7];
 const Browse = () => {
   return (
     <div>
         <Navbar/>
-        <div>
-            <h1>search results {randomJobs.length}</h1>
+        <div className="max-w-7xl mx-auto my-10">
+            <h1 className="font-bold text-xl my-10">search results ({randomJobs.length})</h1>
+            <div className="grid grid-cols-3 gap-4">
+            {
+                randomJobs.map((item,index)=>{
+                    return (
+                        <Job/>
+                    )
+                })
+            }
+            </div>
+            
         </div>
     </div>
   )
