03d093f33489bab2210c543e6614eed3fb52c876
naman katiyar <naman.20233196@mnnit.ac.in>
2025-01-24 22:35:24 +0530

    admin page is still to be cpmplete

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { LogOut, User2 } from "lucide-react";
 import { Button } from "../ui/button";
 import { Avatar, AvatarImage } from "../ui/avatar";
-import { Link,  useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import store from "@/redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
@@ -11,14 +11,15 @@ import { setUser } from "@/redux/authSlice";
 import { toast } from "sonner";
 import axios from "axios";
 
-
 const Navbar = () => {
-  const {user}=useSelector(store=>store.auth);
-  const dispatch=useDispatch();
-  const navigate=useNavigate();
+  const { user } = useSelector((store) => store.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const logoutHandler = async () => {
     try {
-      const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+      const res = await axios.get(`${USER_API_END_POINT}/logout`, {
+        withCredentials: true,
+      });
       if (res.data.success) {
         dispatch(setUser(null)); // Correct Redux dispatch usage
         navigate("/"); // Use the navigate function from useNavigate
@@ -29,7 +30,7 @@ const Navbar = () => {
       toast.error(error.response?.data?.message || "Something went wrong!");
     }
   };
-  
+
   return (
     <div className="bg-white">
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
@@ -40,54 +41,88 @@ const Navbar = () => {
         </div>
         <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/jobs'>Jobs</Link></li>
-            <li><Link to='/browse'>Browse</Link></li>
+            {user && user.role == "recruiter" ? (
+              <>
+                <li>
+                  <Link to="/admin/companies">Companies</Link>
+                </li>
+                <li>
+                  <Link to="/admin/jobs">Jobs</Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/jobs">Jobs</Link>
+                </li>
+                <li>
+                  <Link to="/browse">Browse</Link>
+                </li>
+              </>
+            )}
           </ul>
-          {
-            !user ?(
-              <div className="flex gap-2 items-center">
-                <Link to="/login"><Button variant="outline">Login</Button></Link>
-                <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">Signup</Button></Link>
-              </div>
-            ):(
-              <Popover>
-            <PopoverTrigger asChild>
-              <Avatar className="cursor-pointer">
-                <AvatarImage
-                  src={user?.profile?.profilePhoto}
-                  alt="@shadcn"
-                />
-              </Avatar>
-            </PopoverTrigger>
-            <PopoverContent className="w-80">
-              <div className="flex gap-2 space-y-2">
+          {!user ? (
+            <div className="flex gap-2 items-center">
+              <Link to="/login">
+                <Button variant="outline">Login</Button>
+              </Link>
+              <Link to="/signup">
+                <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
+                  Signup
+                </Button>
+              </Link>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
                 <Avatar className="cursor-pointer">
                   <AvatarImage
                     src={user?.profile?.profilePhoto}
                     alt="@shadcn"
                   />
                 </Avatar>
-                <div>
-                  <h4 className="font-medium">{user?.fullname}</h4>
-                  <p className="text-sm text-muted-foreground">{user?.profile?.bio}</p>
+              </PopoverTrigger>
+              <PopoverContent className="w-80">
+                <div className="flex gap-2 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src={user?.profile?.profilePhoto}
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">{user?.fullname}</h4>
+                    <p className="text-sm text-muted-foreground">
+                      {user?.profile?.bio}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col my-2  text-gray-600">
-                <div className="flex w-fit items-center gap-2 cursor-pointer">
-                  <User2/>
-                  <Button variant="link"><Link to="/profile">view Profile</Link></Button>
+                <div className="flex flex-col my-2  text-gray-600">
+                  {
+                    user &&user.role=="student"&&(
+                      
+                      <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2 />
+                    <Button variant="link">
+                      <Link to="/profile">view Profile</Link>
+                    </Button>
+                  </div>
+                  
+                    )
+                  }
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut />
+                    <Button onClick={logoutHandler} variant="link">
+                      Logout
+                    </Button>
+                  </div>
                 </div>
-                <div className="flex w-fit items-center gap-2 cursor-pointer">
-                  <LogOut/>
-                  <Button onClick={logoutHandler} variant="link">Logout</Button>
-                </div>
-              </div>
-            </PopoverContent>
-          </Popover>
-            )
-          }
-          
+              </PopoverContent>
+            </Popover>
+          )}
         </div>
       </div>
     </div>
